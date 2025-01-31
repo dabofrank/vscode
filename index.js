05d475b77e52991ff35bad757403af3b1fdc5d0c
dabofrank <10994572@qq.com>
2025-01-31 12:23:07 +0800

    Update index.js
    abcabc

--- index.js
+++ index.js
@@ -1,2 +1,146 @@
-first
-asdiquwuhd
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+    <ol type="1">
+        <li>苹果</li>
+        <li>橘子</li>
+        <li>香蕉</li>
+        <li>火龙果</li>
+    </ol>
+
+    <ol type="a">
+        <li>苹果</li>
+        <li>橘子</li>
+        <li>香蕉</li>
+        <li>火龙果</li>
+    </ol>
+
+    <ol type="A">
+        <li>苹果</li>
+        <li>橘子</li>
+        <li>香蕉</li>
+        <li>火龙果</li>
+    </ol>
+
+    <ol type="i">
+        <li>苹果</li>
+        <li>橘子</li>
+        <li>香蕉</li>
+        <li>火龙果</li>
+    </ol>
+
+    <ol type="I">
+        <li>苹果</li>
+        <li>橘子</li>
+        <li>香蕉</li>
+        <li>火龙果</li>
+    </ol>
+
+    <ol>
+        <li>水果</li>
+        <li>
+            蔬菜
+            <ol>
+                <li>白菜</li>
+                <li>油菜</li>
+                <li>辣椒</li>
+                <li>黄瓜</li>
+            </ol>
+        </li>
+        <li>肉类</li>
+    </ol>
+
+
+    <ul type="disc">
+        <li>苹果</li>
+        <li>橘子</li>
+        <li>香蕉</li>
+    </ul>
+    <ul type="circle">
+        <li>苹果</li>
+        <li>橘子</li>
+        <li>香蕉</li>
+    </ul>
+    <ul type="square">
+        <li>苹果</li>
+        <li>橘子</li>
+        <li>香蕉</li>
+    </ul>
+    <ul type="none">
+        <li>苹果</li>
+        <li>橘子</li>
+        <li>香蕉</li>
+    </ul>
+
+
+    <ul>
+        <li>蔬菜</li>
+        <li>
+            水果
+            <ul>
+                <li>苹果</li>
+                <li>香蕉</li>
+                <li>橘子</li>
+            </ul>
+        </li>
+        <li>肉类</li>
+    </ul>
+
+    <ul>
+        <li>Xiaomi手机</li>
+        <li>Redmi手机</li>
+        <li>电视</li>
+    </ul>
+
+
+
+    <table>
+        <tr>
+            <td>单元格</td>
+            <td>单元格</td>
+            <td>单元格</td>
+        </tr>
+        <tr>
+            <td>单元格</td>
+            <td>单元格</td>
+            <td>单元格</td>
+        </tr>
+        <tr>
+            <td>单元格</td>
+            <td>单元格</td>
+            <td>单元格</td>
+        </tr>
+    </table>
+
+
+    <table border="1" width="400px" height="200px">
+        <tr>
+            <td>单元格</td>
+            <td>单元格</td>
+            <td>单元格</td>
+        </tr>
+        <tr>
+            <td>单元格</td>
+            <td>单元格</td>
+            <td>单元格</td>
+        </tr>
+        <tr>
+            <td>单元格</td>
+            <td>单元格</td>
+            <td>单元格</td>
+        </tr>
+        <tr>
+            <td>单元格</td>
+            <td>单元格</td>
+            <td>单元格</td>
+        </tr>
+    </table>
+
+    
+</body>
+</html>
